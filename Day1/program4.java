13318bb94be3db529882fb83063c189f1ae18e42
Pushkar876 <pushkarpandey.1121@gmail.com>
2026-06-04 11:24:45 +0530

    Day1 uplodaded.

--- Day1/program4.java
+++ Day1/program4.java
@@ -7,19 +7,17 @@ import java.util.Scanner;
 public class program4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 0;
-        
-        
+        int n = 0; 
         int count = 0;
-        System.out.print("Ente a number whose number of digits need to be counted: ");
+        System.out.print("Enter a number whose number of digits need to be counted: ");
         n = scanner.nextInt();
         int num = n;
+        // approach
         while(n>0){
             n = n/10;
             count++;
         }
         
-       
         System.out.printf("the number of digits in %d is %d",num,count);
         scanner.close();
         
